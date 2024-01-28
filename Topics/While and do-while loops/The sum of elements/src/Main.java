219aeff3add3d20aa8564fdd06c634818ca82b9e
Bjoern Wilke <bjoern.wilke@posteo.de>
2024-01-28 12:07:05 +0100

    Initial commit

---
+++ Topics/While and do-while loops/The sum of elements/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number = 0;
+        do {
+            number = scanner.nextInt();
+            sum += number;
+        } while (number != 0);
+        System.out.println(sum);
+    }
+}
